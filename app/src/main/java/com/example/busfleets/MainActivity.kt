a861bfcaeebb755473bfe4e5421dfb488058ee2a
Alibek Kerimkulov <kerimkulov.ali@mail.ru>
2020-10-02 23:16:07 +0600

    updated

--- app/src/main/java/com/example/busfleets/MainActivity.kt
+++ app/src/main/java/com/example/busfleets/MainActivity.kt
@@ -42,7 +42,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-//        insertSchedule()
+        var c = 1
+        if(c == 1){
+            insertSchedule()
+            c += 1
+        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -64,25 +68,25 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-//    @RequiresApi(Build.VERSION_CODES.O)
-//    private fun insertSchedule(){
-//        val depDate1 = LocalDate.now()
-//        val depDate2 = LocalDate.now()
-//        val arrDate1 = LocalDate.now().plusDays(1)
-//        val arrDate2 = LocalDate.now().plusDays(1)
-//        val arrDate3 = LocalDate.now().plusDays(2)
-//        val arrDate4 = LocalDate.now().plusDays(2)
-//
-//        AsyncTask.execute{
-//            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(
-//                Schedule(fromCity = "Асыката", toCity = "Алматы", departureTime = "18:39", arrivalTime = "6:10", departureDate = depDate1.toString(), arrivalDate = arrDate1.toString(), busNum = "KZ 888 KN 02", placeNum = 32, model = "Yokohama", image = R.drawable.busf)
-//            )
-//            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(Schedule(fromCity = "Астана", toCity = "Алматы", departureTime = "19:20", arrivalTime = "4:36", departureDate = depDate2.toString(), arrivalDate = arrDate2.toString() ,busNum = "KZ 998 KN 03", placeNum = 46,  model = "VolVa", image = R.drawable.buss))
-//            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(Schedule(fromCity = "Актау", toCity = "Атырау", departureTime = "15:30", arrivalTime = "9:40", departureDate = arrDate1.toString(), arrivalDate = arrDate3.toString(), busNum = "KZ 468 KN 02", placeNum = 48,  model = "MAZDARA", image = R.drawable.bust))
-//            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(Schedule(fromCity = "Кокшетау", toCity = "Жезказган", departureTime = "18:19", arrivalTime = "7:20", departureDate = arrDate2.toString(), arrivalDate = arrDate4.toString(), busNum = "KZ 346 KN 02", placeNum = 52,  model = "Yokohama", image = R.drawable.busfo))
-//
-//        }
-//    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun insertSchedule(){
+        val depDate1 = LocalDate.now()
+        val depDate2 = LocalDate.now()
+        val arrDate1 = LocalDate.now().plusDays(1)
+        val arrDate2 = LocalDate.now().plusDays(1)
+        val arrDate3 = LocalDate.now().plusDays(2)
+        val arrDate4 = LocalDate.now().plusDays(2)
+
+        AsyncTask.execute{
+            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(
+                Schedule(fromCity = "Асыката", toCity = "Алматы", departureTime = "18:39", arrivalTime = "6:10", departureDate = depDate1.toString(), arrivalDate = arrDate1.toString(), busNum = "KZ 888 KN 02", placeNum = 32, model = "Yokohama", image = R.drawable.busf)
+            )
+            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(Schedule(fromCity = "Астана", toCity = "Алматы", departureTime = "19:20", arrivalTime = "4:36", departureDate = depDate2.toString(), arrivalDate = arrDate2.toString() ,busNum = "KZ 998 KN 03", placeNum = 46,  model = "VolVa", image = R.drawable.buss))
+            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(Schedule(fromCity = "Актау", toCity = "Атырау", departureTime = "15:30", arrivalTime = "9:40", departureDate = arrDate1.toString(), arrivalDate = arrDate3.toString(), busNum = "KZ 468 KN 02", placeNum = 48,  model = "MAZDARA", image = R.drawable.bust))
+            ApplicationDatabase.getInstance(applicationContext)!!.getScheduleDao().insertSchedule(Schedule(fromCity = "Кокшетау", toCity = "Жезказган", departureTime = "18:19", arrivalTime = "7:20", departureDate = arrDate2.toString(), arrivalDate = arrDate4.toString(), busNum = "KZ 346 KN 02", placeNum = 52,  model = "Yokohama", image = R.drawable.busfo))
+
+        }
+    }
 
 
 }
